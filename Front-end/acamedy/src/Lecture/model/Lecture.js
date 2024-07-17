e31c7ef0b4a1bf9a54c5698286f2fe2e63de84d7
seohyun-admin <sh54991210@gmail.com>
2024-07-17 21:32:43 +0900

    21:30 / front - main sample page 구현 중 + 폴더랑 js 파일 만듦

---
+++ Front-end/acamedy/src/Lecture/model/Lecture.js
@@ -0,0 +1,10 @@
+export default class Lecture {
+    constructor(lno, l_name, l_category, l_classroom, regDate, modDate) {
+        this.lno = lno;
+        this.l_name = l_name;
+        this.l_category = l_category;
+        this.l_classroom = l_classroom;
+        this.regDate = regDate;
+        this.modDate = modDate;
+    }
+}
