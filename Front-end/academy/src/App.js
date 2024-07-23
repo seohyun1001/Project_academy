683cbe86565a94ef6aae9cbec0bf7903265aa735
seohyun1001 <sh54991210@gmail.com>
2024-07-23 17:50:39 +0900

    main 페이지 작업중

--- Front-end/academy/src/App.js
+++ Front-end/academy/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/member/Register/Register';
 import Login from './components/member/Login/Login'; // Login 컴포넌트를 불러옵니다
+import Header from './components/Header';
 
 const App = () => {
     return (
         <Router>
+            <Header />
             <Routes>
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 {/* 다른 라우트들도 추가할 수 있습니다 */}
+                
             </Routes>
         </Router>
     );
