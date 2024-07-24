b8ecdfd21309ab43fe1cc452f10dbaca869fdf32
BOOK-QN42CQM9L8\rbdnj <kmmh99@naver.com>
2024-07-24 14:38:30 +0900

    계속 병합중

--- Front-end/academy/src/App.js
+++ Front-end/academy/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import Register from './components/member/memberRegister/MemberRegister';
-import Login from './components/member/memberLogin/MemberLogin';
-import StudentList from './components/student/StudentList';
-=======
-import Register from './components/member/Register/Register';
-import Login from './components/member/Login/Login'; // Login 컴포넌트를 불러옵니다
+import Login from './components/member/memberLogin/MemberLogin'; // Login 컴포넌트를 불러옵니다
 import Header from './components/Header';
->>>>>>> origin/ksh
 
 const App = () => {
     return (
         <Router>
             <Header />
             <Routes>
-                <Route path="/MemberRegister" element={<Register />} />
-                <Route path="/MemberLogin" element={<Login />} />
-                <Route path="/student" element={<StudentList />} />
+                <Route path="/memberRegister" element={<Register />} />
+                <Route path="/memberLogin" element={<Login />} />
+                {/* <Route path="/student" element={<StudentList />} /> */}
                 {/* 다른 라우트들도 추가할 수 있습니다 */}
-                
             </Routes>
         </Router>
     );
